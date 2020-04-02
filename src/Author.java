023ba4d6a5e2e8abeb7068bd3a24662d77d5a6f5
viviancan <vcnales@gmail.com>
2020-04-01 21:03:38 -0500

    made a better fix

--- src/Author.java
+++ src/Author.java
@@ -4,12 +4,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
+//https://docs.oracle.com/javase/tutorial/java/generics/rawTypes.html
 public class Author extends Person {
-    private List books;
+    private ArrayList<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
@@ -29,7 +31,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
